2c2cf18d9340007925d2893b28405b57e8948e0d
mahdiam1221 <mahdiam1221@gmail.com>
2016-03-08 22:41:25 +0330

    Update plugins.lua

--- plugins/plugins.lua
+++ plugins/plugins.lua
@@ -208,7 +208,7 @@ return {
           "!plugins reload : reloads all plugins." },
           },
   patterns = {
-    "^!plugins$",
+    "^پلاگینز$",
     "^!plugins? (enable) ([%w_%.%-]+)$",
     "^!plugins? (disable) ([%w_%.%-]+)$",
     "^!plugins? (enable) ([%w_%.%-]+) (chat)",
