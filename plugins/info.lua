9855e12bddcc8069b791d5bb63ac6e8d63694272
mahdiam1221 <mahdiam1221@gmail.com>
2016-03-08 23:03:54 +0330

    Update info.lua

--- plugins/info.lua
+++ plugins/info.lua
@@ -216,18 +216,18 @@ end
 return {
   description = 'Know your information or the info of a chat members.',
   usage = {
-	'!info: Return your info and the chat info if you are in one.',
+	'اینفو: Return your info and the chat info if you are in one.',
 	'(Reply)!info: Return info of replied user if used by reply.',
 	'!info <id>: Return the info\'s of the <id>.',
-	'!info @<user_name>: Return the member @<user_name> information from the current chat.',
-	'!setrank <userid> <rank>: change members rank.',
-	'(Reply)!setrank <rank>: change members rank.',
+	'اینفو @<user_name>: Return the member @<user_name> information from the current chat.',
+	'ست رانک <userid> <rank>: change members rank.',
+	'(Reply)ست رانک <rank>: change members rank.',
   },
   patterns = {
-	"^[/!]([Ii][Nn][Ff][Oo])$",
-	"^[/!]([Ii][Nn][Ff][Oo]) (.*)$",
-	"^[/!]([Ss][Ee][Tt][Rr][Aa][Nn][Kk]) (%d+) (.*)$",
-	"^[/!]([Ss][Ee][Tt][Rr][Aa][Nn][Kk]) (.*)$",
+	"^(اینفو)$",
+	"^(اینفو) (.*)$",
+	"^(ست رانک) (%d+) (.*)$",
+	"^(ست رانک) (.*)$",
   },
   run = run
 }
